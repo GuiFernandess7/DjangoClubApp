7fd0138a3568810c20093d63d1bd707431550d4b
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-20 20:42:33 -0300

    key updated to settings

--- club_website/settings.py
+++ club_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from key import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u!#wop^_c0@6if0tb6q*z263#@ov+moo#nu4rj-%n-a^drh82='
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
