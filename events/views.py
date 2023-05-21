e190a146c8b50a1eb37ceb02f5ed1f1adf2259c4
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-20 22:50:09 -0300

    All events function added

--- events/views.py
+++ events/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Event
 
 def home(request):
     return render(request, 'events/home.html', {})
+
+def all_events(request):
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', 
+                  {"event_list": events})
