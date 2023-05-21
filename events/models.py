f585890406126211b0805204bcbc7c053581e1ad
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-20 23:28:36 -0300

    Default user model added

--- events/models.py
+++ events/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Venue(models.Model):
     name = models.CharField(max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length = 15)
-    phone = models.CharField('Contact Phone', max_length=25)
-    web = models.URLField()
-    email_address = models.EmailField() 
+    phone = models.CharField('Contact Phone', max_length=25, blank=True)
+    web = models.URLField(blank=True)
+    email_address = models.EmailField(blank=True) 
 
     def __str__(self) -> str:
         return self.name
@@ -23,8 +24,9 @@ class Event(models.Model):
     name = models.CharField(max_length=120)
     event_date = models.DateTimeField()
     venue = models.ForeignKey(Venue, blank=True, null=True,
-                              on_delete=models.SET_NULL)
-    manager = models.CharField(max_length=60)
+                                on_delete=models.SET_NULL)
+    manager = models.ForeignKey(User, blank=True, null=True, 
+                                on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(ClubUser, blank=True)
 
