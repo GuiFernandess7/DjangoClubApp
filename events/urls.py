ae3f785caecffca699a304df0c56ea6274311c1d
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-21 17:38:30 -0300

    Create venue url route added

--- events/urls.py
+++ events/urls.py
@@ -5,4 +5,5 @@ from events.views import *
 urlpatterns = [
     path('', home, name="home"),
     path('events/', all_events, name="event_list"),
+    path('add_venue', add_venue, name='add_venue'),
 ]
