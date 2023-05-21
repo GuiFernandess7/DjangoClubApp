40e423d0d0991fc4b0c01315f2f174d67f49726e
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-21 17:38:43 -0300

    views function for create venue added

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Event
+from .forms import VenueForm
 
 def home(request):
     return render(request, 'events/home.html', {})
@@ -8,3 +9,22 @@ def all_events(request):
     events = Event.objects.all()
     return render(request, 'events/event_list.html', 
                   {"event_list": events})
+
+def add_venue(request):
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/add_venue?submitted=True")
+    else:
+        if 'submitted' in request.GET:
+            submitted = True
+        form = VenueForm()
+
+    context = {
+        'form': form,
+        'submitted': submitted
+    }
+    form = VenueForm()
+    return render(request, 'events/add_venue.html', context)
