5462fb5fb84d6055621696b62e40131fa3be74e4
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-21 18:38:59 -0300

    List and venue function detail added

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Event
+from .models import *
 from .forms import VenueForm
 
 def home(request):
@@ -28,3 +28,13 @@ def add_venue(request):
     }
     form = VenueForm()
     return render(request, 'events/add_venue.html', context)
+
+def list_venues(request):
+    venue_list = Venue.objects.all()
+    return render(request, 'events/venues.html', 
+                  {'venue_list': venue_list})
+
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(id=venue_id)
+    return render(request, 'events/show_venue.html', 
+                  {'venue': venue})
