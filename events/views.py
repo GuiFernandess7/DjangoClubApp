8f9509e1a3de0fa2bd1f9a4f7f5ba8d5483bdbdc
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-26 18:58:52 -0300

    update event view added

--- events/views.py
+++ events/views.py
@@ -80,3 +80,13 @@ def add_event(request):
     }
     form = EventForm()
     return render(request, 'events/add_event.html', context)
+
+def update_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    form = EventForm(request.POST or None, instance=event)
+    if form.is_valid():
+        form.save()
+        return redirect('list-events')
+    
+    return render(request, 'events/update_event.html', 
+                  {'event': event, 'form': form})
