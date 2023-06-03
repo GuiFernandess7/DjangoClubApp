34681b6cc004db493e786f6e5d2b8e9cab29cb9c
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-06-03 13:25:11 -0300

    login view added

---
+++ members/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, logout, login as auth_login
+from django.contrib import messages
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, 
+                            password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("There was a error logging In"))
+            return redirect('login')
+    else:
+        return render(request, 'auth/login.html')
