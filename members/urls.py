0d6134191bde4e8650787c74d8bebed9605f4503
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-06-03 15:54:42 -0300

    register user path added

--- members/urls.py
+++ members/urls.py
@@ -3,5 +3,6 @@ from members.views import *
 
 urlpatterns = [
    path('login', login, name="login"),
-   path('logout', logout_user, name='logout')
+   path('logout', logout_user, name='logout'),
+   path('register_user', register_user, name='register_user'),
 ]
