ceb457f9afd84f5645d90d35f9c482f0e1f2d27d
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-06-03 17:18:53 -0300

    register user fields updated

--- members/forms.py
+++ members/forms.py
@@ -1,12 +1,21 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name =  forms.CharField(max_length=50)
-    last_name  =  forms.CharField(max_length=50)
-    address = forms.CharField(max_length=200)
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control'}
+    ))
+    first_name =  forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
+    last_name  =  forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
+    address = forms.CharField(max_length=200, widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
 
     class Meta:
         model = User
@@ -16,4 +25,11 @@ class RegisterUserForm(UserCreationForm):
                   'email', 
                   'password1',
                   'password2',
-                  'address')
+                  'address'
+        )
+    
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
